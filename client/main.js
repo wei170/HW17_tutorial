2c735024becc9c127ce1298d9f9ae3f3531c0d8f
wei170 <guocheng@guochengwei.com>
2017-08-21 11:41:35 -0400

    Add MainLayout and MainLayout

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
+import '../imports/startup/lib/routes.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // code to run on client at startup
 });
